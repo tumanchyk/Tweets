45936e4358d6afc18033d2d90a1299b796749001
Tetiana Tsar <protetsar@gmail.com>
2023-05-09 19:45:34 +0300

    add homepage

---
+++ docs/assets/Home-3e670ddc.js
@@ -0,0 +1,34 @@
+import{n as t,L as o,j as e}from"./index-58856731.js";const r=t(o)`
+display: flex;
+justify-content: center;
+align-items: center;
+padding: 14px 28px;
+width: 196px;
+height: 50px;
+background-color: #EBD8FF;
+box-shadow: 0px 3.43693px 3.43693px rgba(0, 0, 0, 0.25);
+border-radius: 10px;
+border: none;
+cursor: pointer;
+font-weight: 600;
+font-size: 22px;
+line-height: 22px;
+text-transform: uppercase;
+color: #373737;
+margin: 0 auto;
+margin-top: 40px;
+text-decoration: none;
+
+&:hover,
+&:focus{
+background-color: #5432A0;
+color: #fff;
+}
+`,n=t.h1`
+color: #75B79F;
+width: 700px;
+margin: 0 auto;
+font-size: 40px;
+margin-top: 150px;
+text-align: center;
+`,s=()=>e.jsxs(e.Fragment,{children:[e.jsx(n,{children:"Follow your favorite super hero and read their tweets"}),e.jsx(r,{to:"/tweets",children:"Let's  Tweets"})]});export{s as default};
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { lazy } from 'react';
 import { Routes, Route } from "react-router-dom";
 import { Layout } from "./components/Layout/Layout";
+
 const Home = lazy(()=> import('./pages/Home/Home'));
 const Tweets = lazy(()=> import('./pages/Tweets'));
 
